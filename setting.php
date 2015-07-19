d97d602b222463a6d078048d05d4518fe1386b85
Christopher Boyd <chris@boyddesignstudio.com>
2015-07-19 09:30:23 -0400

    Initial commit

---
+++ setting.php
@@ -0,0 +1,22 @@
+<?php 
+define('CNF_APPNAME','Boyd Design Studio');
+define('CNF_APPDESC','PHP Application Builder ');
+define('CNF_COMNAME','Christopher Boyd');
+define('CNF_EMAIL','chris@boyddesignstudio.com');
+define('CNF_METAKEY','my site, my company');
+define('CNF_METADESC','Write description for your site');
+define('CNF_GROUP','3');
+define('CNF_ACTIVATION','confirmation');
+define('CNF_MULTILANG','1');
+define('CNF_LANG','en');
+define('CNF_REGIST','true');
+define('CNF_FRONT','true');
+define('CNF_RECAPTCHA','false');
+define('CNF_THEME','sximone');
+define('CNF_RECAPTCHAPUBLICKEY','');
+define('CNF_RECAPTCHAPRIVATEKEY','');
+define('CNF_MODE','production');
+define('CNF_LOGO','backend-logo.png');
+define('CNF_ALLOWIP','');
+define('CNF_RESTRICIP','192.116.134 , 194.111.606.21 ');
+?>
